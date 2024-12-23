45362b429e16e758d2c25ee066f7f53ed96a3524
TryAngle <45734252+TriedAngle@users.noreply.github.com>
2024-12-23 16:40:05 +0100

    Add: file following and some key bindings to Neotree

--- nvim/lua/tryangle/lazy/neotree.lua
+++ nvim/lua/tryangle/lazy/neotree.lua
@@ -2,9 +2,48 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+        "3rd/image.nvim",
+    },
+    opts = {
+        window = {
+            position = "left",
+            width = 30,
+            mapping_options = {
+                noremap = true,
+                nowait = true,
+            },
+            mappings = {
+                ["<space>"] = {
+                    "toggle_node",
+                    nowait = false,
+                },
+            },
+            preview_width = 50,
+            preview = true,
+        },
+        filesystem = {
+            follow_current_file = {
+                enabled = true,
+            },
+            group_empty_dirs = false,
+            hijack_netrw_behavior = "open_default",
+            use_libuv_file_watcher = true,
+            window = {
+                mappings = {
+                    ["<bs>"] = "navigate_up",
+                    ["."] = "set_root",
+                    ["H"] = "toggle_hidden",
+                    ["/"] = "fuzzy_finder",
+                    ["D"] = "fuzzy_finder_directory",
+                    ["f"] = "filter_on_submit",
+                    ["<c-x>"] = "clear_filter",
+                    ["[g"] = "prev_git_modified",
+                    ["]g"] = "next_git_modified",
+                }
+            }
+        },
     }
 }
